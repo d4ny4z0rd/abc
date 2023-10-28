70439796b68a5051f95dac4b70d9a1ca24daa105
Prabhav Dixit <42492143+prabhavdixit@users.noreply.github.com>
2023-10-28 23:46:55 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-3">
-      This website is designed by GTCoding © 2023
+      This website is designed by Prabhav Dixit © 2023
     </footer>
   );
 }
